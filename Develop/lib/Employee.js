f532f73912849cc430fafc3d778c4ee3f15e91fd
Grace Kuo <gkuo.github@gmail.com>
2021-01-04 11:36:38 -0700

    code for employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, role, email) {
+    this.name = name;
+    this.role = role;
+    this.email = email;
+  }
+
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.role;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+
+module.exports = Employee;
