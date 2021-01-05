02af14b3a50738e04e41c93c60afd53a650b1da2
Grace Kuo <gkuo.github@gmail.com>
2021-01-05 12:45:21 -0700

    functions for intern

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(name, role, email, school) {
+    super(name, role, email);
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+  getRole() {
+    return "Intern";
+  }
+}
+
+module.exports = Intern;
