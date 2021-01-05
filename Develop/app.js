d0afe610422caa04e10918b836e6f46f2453d347
Grace Kuo <gkuo.github@gmail.com>
2021-01-05 12:57:11 -0700

    functions to add another employee

--- Develop/app.js
+++ Develop/app.js
@@ -76,42 +76,48 @@ function main() {
 }
 
 async function createEngineer(name, role, email) {
-    const response = await inquirer.prompt([
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "github",
+    },
+  ]);
+
+  return new Engineer(name, role, email, response.github);
+}
+
+async function createIntern(name, role, email) {
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "Where do you attend University?",
+      name: "university",
+    },
+  ]);
+  return new Intern(name, id, email, response.university);
+}
+function addAnotherEmployee() {
+  inquirer
+    .prompt([
       {
-        type: "input",
-        message: "What is your Github username?",
-        name: "github",
+        type: "confirm",
+        message: "Would you like to add another employee?",
+        name: "addEmployee",
       },
-    ]);
-  
-    return new Engineer(name, role, email, response.github);
-  }
-  
-  async function createIntern(name, id, email) {
-    const response = await inquirer.prompt([
-      {
-        type: "input",
-        message: "Where do you attend University?",
-        name: "university",
-      },
-    ]);
-  
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    ])
+    .then((response) => {
+      if (response.addEmployee) {
+        main();
+      } else {
+        const data = render(employees);
+        //fs function
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR);
+        }
+        fs.writeFile(outputPath, data, (err) => {
+          return err ? console.log(err) : console.log("Success!");
+        });
+      }
+    });
+}
